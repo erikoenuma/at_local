04e3959dd3d7ee96ff3f056a76f6eb08f0312056
erikoenuma <koenuma.eri@gmail.com>
2022-06-16 02:36:07 +0900

    unicorn修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,25 @@ set :rbenv_ruby, '3.0.1'
 # エラーのログレベル
 set :log_level, :info
 
-after 'deploy:published', 'deploy:seed'   # 9
-after 'deploy:finished', 'deploy:restart'   # 10
+after 'deploy:published', 'deploy:seed'
+after 'deploy:finished', 'deploy:restart'
+
+namespace :deploy do
+    desc 'Run seed'
+    task :seed do
+        on roles(:db) do
+        with rails_env: fetch(:rails_env) do
+            within current_path do
+            execute :bundle, :exec, :rake, 'db:seed'
+            end
+        end
+        end
+    end
+    desc 'Restart application'
+    task :restart do
+        invoke 'unicorn:restart'
+    end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

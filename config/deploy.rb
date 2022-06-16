ad97eead24fc4c2765cc17e96ee85e2be1ba98fa
erikoenuma <koenuma.eri@gmail.com>
2022-06-16 12:42:46 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ lock "~> 3.17.0"
 set :application, "at_local"
 set :repo_url, "https://github.com/erikoenuma/at_local.git"
 set :linked_files, %w{config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads} 
 # masterkeyを本番環境で読み取る
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 # releasesディレクトリ内で保持するバージョンの個数を設定

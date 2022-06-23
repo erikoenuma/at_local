c2dfbd5728ac27f7bc408ff55cebf9df8d286939
erikoenuma <koenuma.eri@gmail.com>
2022-06-23 23:35:05 +0900

    selectタグをやめた

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,15 +1,3 @@
 module ShopsHelper
 
-    # cancelable_days_stringの値から適切な文言を返す
-    def cancel_string(days)
-        case days
-        when 0 then
-            "当日まで可能"
-        when 8 then
-            "キャンセル不可"
-        else
-            "#{days}日前まで可能"
-        end
-    end
-
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,10 +5,11 @@ class Shop < ApplicationRecord
     has_one_attached :image
     has_many :orders
 
+    enum cancelable_days_before: [ :today, :one, :two, :three, :four, :five, :six, :seven, :not_allowed ]
+
     validates :name, presence: true, length: { maximum: 30 }
     validates :address, length: { maximum: 255}
     validates :access, length: { maximum: 255 }
     validates :introduction, length: { maximum: 1000, too_long: "最大%{count}文字まで使えます"}
-    validates :cancelable_days_before, numericality: { in: 0..8, allow_blank: true }
     validates :private, inclusion: {in: [true, false]}
 end
